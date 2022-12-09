4e7d6a00f27fbf24f598e212cf8342736b5fb5c8
Marina_Arnaut <skital87@mail.ru>
2022-12-09 21:15:56 +1000

    case3

---
+++ case3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int i = 1;
+
+Console.WriteLine($"Таблица кубов числа {N}, от 1 до {N} ");
+while (i <= N)
+{
+    double result = Math.Pow(i, 3);
+    Console.Write(result + " ");
+    i++;
+}
